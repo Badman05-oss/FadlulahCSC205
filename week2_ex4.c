640a53d7ffa413f0083b2c44b68266bcfd0ebaab
Badman05-oss <82161670+Badman05-oss@users.noreply.github.com>
2021-12-12 01:25:52 +0100

    Add files via upload

---
+++ week2_ex4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char myInitial = 'F';
+    char myName[] = "fadil";
+    printf("Your name is = %s\n", myName);
+    printf("The initial of your firstname is = %c\n", myInitial);
+    return 0;
+}
